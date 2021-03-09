59564e3f691195df3e12282922f5934023f37f38
andrebclark <andre@cosmicdivision.dev>
2021-03-08 17:09:21 -0800

    add shades of purple zsh theme

--- scripts/pacman/oh-my-zsh-install.sh
+++ scripts/pacman/oh-my-zsh-install.sh
@@ -8,3 +8,6 @@ fi
 
 pacman -S --noconfirm oh-my-zsh
 
+curl -fsSL "https://raw.githubusercontent.com/ahmadawais/Shades-of-Purple-iTerm2/master/shades-of-purple.zsh-theme" --output /usr/share/oh-my-zsh/themes/shades-of-purple.zsh-theme
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="shades-of-purple"/' $HOME/.zshrc
+echo 'installed shades of purple theme'
